50512e27541c84f587ccf4e48f79bab623b17f8e
andrew.romero <am28romero@gmail.com>
2025-05-10 01:48:58 -0500

    CICD fix test 7

--- obj/Debug/net8.0/PhotoShare.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhotoShare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoShare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a54b40cf2d7a2b98924f05e6e52f47a7523c2df3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f604b7d100d4b49f1c1cc62922bbc5beac811a90")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoShare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoShare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
