03b3d23a396d61bef39a1ed48fd8d2c34b96d6e9
andrew.romero <am28romero@gmail.com>
2025-05-12 14:09:53 -0500

    fix logout functionality

---
+++ Components/Account/Logout.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using PhotoShare.Data;
+
+public class LogoutModel : PageModel
+{
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public LogoutModel(SignInManager<ApplicationUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    [BindProperty]
+    public string? ReturnUrl { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await _signInManager.SignOutAsync();
+        return Redirect(ReturnUrl ?? "/");
+    }
+}
